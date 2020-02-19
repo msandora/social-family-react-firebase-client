7aef21a3d1e4f9be4c8089f7ed5b3ea5126fd587
msandora <mark.sandora@gmail.com>
2020-02-19 01:34:13 -0500

    add gallery demo

---
+++ src/data/gallery.js
@@ -0,0 +1,34 @@
+
+//  The example data is structured as follows:
+
+import image from '../images/no-img.png';
+// [etc...]
+
+const tileData = [
+  {
+    postId: '1',
+    img: image,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    postId: '2',
+    img: image,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    postId: '3',
+    img: image,
+    title: 'Image',
+    author: 'author',
+  },
+  {
+    postId: '4',
+    img: image,
+    title: 'Image',
+    author: 'author',
+  }
+];
+ 
+export default tileData;
--- src/redux/types.js
+++ src/redux/types.js
@@ -10,7 +10,9 @@ export const LOADING_UI = 'LOADING_UI';
 export const CLEAR_ERRORS = 'CLEAR_ERRORS';
 export const LOADING_DATA = 'LOADING_DATA';
 export const STOP_LOADING_UI = 'STOP_LOADING_UI';
+
 // Data reducer types
+// Screams
 export const SET_SCREAMS = 'SET_SCREAMS';
 export const SET_SCREAM = 'SET_SCREAM';
 export const LIKE_SCREAM = 'LIKE_SCREAM';
@@ -18,7 +20,7 @@ export const UNLIKE_SCREAM = 'UNLIKE_SCREAM';
 export const DELETE_SCREAM = 'DELETE_SCREAM';
 export const POST_SCREAM = 'POST_SCREAM';
 export const SUBMIT_SCREAM_COMMENT = 'SUBMIT_SCREAM_COMMENT';
-
+// Recipes
 export const SET_RECIPES = 'SET_RECIPES';
 export const SET_RECIPE = 'SET_RECIPE';
 export const LIKE_RECIPE = 'LIKE_RECIPE';
